d5dd05146a4648dc913f7f920a392f797b0179e4
Carlos <gordillod274@gmail.com>
2024-12-09 08:54:37 -0500

    Inicializacion de servicio web en node.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+// Protocolo de intercambio
+var http = require('http');  
 
-var http = require('http');
+var server = http.createServer();
 
-var server = http.createServer(function (req, resp) {
+// Servicio Web
+function Gordillo(req, resp) {
     resp.writeHead(200, { 'Content-Type': 'text/plain' });
-    resp.write('CalosGordillo: Este es el mensaje que debe de recibir el movil.');  
-    resp.end();
-});
+    resp.write('Carlos Gordillo, de 20 años, es estudiante de quinto nivel en la carrera de Desarrollo de Software. Este texto corresponde al examen de la asignatura Herramientas para el Desarrollo de Software y presenta un ejemplo práctico de cómo levantar un servicio web.');
 
-server.listen(3000, function () {
-    console.log('Servidor escuchando en el puerto 3000, funciona!!!'); 
+    resp.end();
+}
+
+server.on('request', Gordillo);
+
+// El servidor escucha en el puerto 3000
+server.listen(3000, function() {
+    console.log('La solicitud fue realizada por el puerto 3000');
 });
